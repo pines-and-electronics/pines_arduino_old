43541a28a6c164beb07488e4370ae36219914e2b
MarcusJones <jones.0bj3@gmail.com>
2019-04-02 21:17:09 +0200

    add gps test

---
+++ test_gps/src/test_gps.cpp
@@ -0,0 +1,16 @@
+
+/*
+Adafruit GPS Library
+====================
+#ID: 20
+An interrupt-based GPS library for no-parsing-required use
+
+Keywords: sensors
+Compatible frameworks: Arduino
+Compatible platforms: Atmel AVR, Atmel SAM, Espressif 32, Espressif 8266, Intel ARC32, Microchip PIC32, Nordic nRF51, Nordic nRF52, ST STM32, Teensy, TI MSP430
+Authors: Adafruit
+*/
+
+// 
+// pio lib install "Adafruit GPS Library"
+
