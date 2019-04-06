647239f620f93e9a99a71ea63490156fb1d83807
MarcusJones <jones.0bj3@gmail.com>
2019-04-06 09:20:30 +0200

    update remove vscode dirs

---
+++ test_i2c/src/arduino_i2c_0x08.cpp
@@ -0,0 +1,17 @@
+/* Minimum example for i2c bus
+Change the address number and upload for i2c testing
+*/
+#include <Arduino.h>
+#include <Wire.h>
+
+// I2C settings
+int i2c_address = 0x08;
+
+void setup() {
+  // I2C setup
+  Wire.begin(i2c_address);      // join i2c bus with address #8
+}
+
+void loop()
+{
+}
